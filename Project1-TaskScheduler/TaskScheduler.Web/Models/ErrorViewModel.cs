446a9733fc42c72c3e0d3f51c0b6c9214b3bea0a
leffallc <leffall.carl@gmail.com>
2026-01-09 14:24:44 -0600

    feat: create initial ASP.NET Core MVC project

---
+++ Project1-TaskScheduler/TaskScheduler.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskScheduler.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
