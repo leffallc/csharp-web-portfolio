2242a863d2bc84b0023cbc245ac55e54fef02e59
leffallc <leffall.carl@gmail.com>
2026-02-16 04:43:14 -0600

    feat: create initial ThuTasty ASP.NET Core MVC project

---
+++ Project2-ThuTasty/src/ThuTasty.Web/ThuTasty.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThuTasty.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
