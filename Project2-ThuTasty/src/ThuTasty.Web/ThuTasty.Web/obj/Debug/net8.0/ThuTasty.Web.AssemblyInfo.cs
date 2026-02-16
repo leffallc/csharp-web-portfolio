d7362dfad8a227057cc390a7415fef2898cebea8
leffallc <leffall.carl@gmail.com>
2026-02-16 04:52:51 -0600

    feat: add ThuTasty brand theme and layout styling

--- Project2-ThuTasty/src/ThuTasty.Web/ThuTasty.Web/obj/Debug/net8.0/ThuTasty.Web.AssemblyInfo.cs
+++ Project2-ThuTasty/src/ThuTasty.Web/ThuTasty.Web/obj/Debug/net8.0/ThuTasty.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThuTasty.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d922510417055e94eae78978607019a0824cc92c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2242a863d2bc84b0023cbc245ac55e54fef02e59")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThuTasty.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThuTasty.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
